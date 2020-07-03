5dc7273e88720128d6aac04241b7c4e56c5ac732
Yves Goldberg <admin@ygol.com>
2020-07-03 14:51:33 +0300

    [ADD] app website_menu_access_groups

---
+++ website_menu_access_groups/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Website Menu Access Groups",
+    'version': "1.0.1",
+    'author': "Sythil Tech",
+    'category': "Tools",
+    'summary':'Define which user groups can access a website menu',
+    'description':'Define which user groups can access a website menu',
+    'license':'LGPL-3',
+    'data': [
+        'views/website_page_views.xml',
+    ],
+    'depends': ['website'],
+    'installable': True,
+}
